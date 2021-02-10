d258bf94329aac57e74425c5cbccb9aaf4aad9a7
Mohammed Sohail <itscrazybob@gmail.com>
2021-02-10 17:24:45 +0400

    wip

--- src/PolynomialTester.java
+++ src/PolynomialTester.java
@@ -1,6 +1,6 @@
 /**
  * Lab01
- * Instructions for Lab01 are in README.md file located in the root directory of Lab01
+ * Instructions for Lab01 are in CS102_Lab01.pdf file located in the root directory of Lab01
  * Revisions can be seen on the following GitHub URL: https://github.com/thecrazybob/CS102-lab01
  * Style Guidelines: http://www.cs.bilkent.edu.tr/~adayanik/cs101/practicalwork/styleguidelines.htm
  *
